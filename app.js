76de782fc31eaceb087cb232fef9420568683733
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-15 22:07:30 +0100

    basic express app

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const express = require("express")
-const app = express()
+require("dotenv").config();
 
+const express = require("express");
+const app = express();
+
+// Db
+const connectDB = require("./db/connect");
+
+// start
+const port = process.env.PORT;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(port, () => {
+      console.log(`Listening on port : ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
