3b5144bdfb04934a0cd746ae40063a2f47ef5e40
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-15 22:09:45 +0100

    basic express app

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// middleware
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+
 // Db
 const connectDB = require("./db/connect");
+
+// *********************************
+// *********************************
+
+app.use(notFound);
+app.use(errorHandler);
 
 // start
 const port = process.env.PORT;
