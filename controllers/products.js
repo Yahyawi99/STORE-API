5d36f54a94941113c7c764569eb31f95a81e46da
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-15 22:58:33 +0100

    working on controllers

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,16 @@
+const Product = require("../models/product");
+const { StatusCodes } = require("http-status-codes");
+
 const getAllProducts = async (req, res) => {
-  res.send("get all");
+  const products = await Product.find(req.query);
+
+  res.status(StatusCodes.OK).json({ products, count: products.length });
 };
 
 const getAllProductsStatic = async (req, res) => {
-  res.send("get all testing route");
+  const products = await Product.find({ featured: true });
+
+  res.status(StatusCodes.OK).json({ products, count: products.length });
 };
 
 module.exports = {
--- populate.js
+++ populate.js
@@ -18,6 +18,7 @@ const start = async () => {
     process.exit(0);
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
 };
 
