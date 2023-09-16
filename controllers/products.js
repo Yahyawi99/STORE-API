413ae650f39f31b9bdb95e6f6151605c4ed003cb
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-16 17:35:12 +0100

    implement search

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,33 @@ const Product = require("../models/product");
 const { StatusCodes } = require("http-status-codes");
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, company, name } = req.query;
+
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company && ["ikea", "liddy", "marcos", "caressa"].includes(company)) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  const products = await Product.find(queryObject);
 
   res.status(StatusCodes.OK).json({ products, count: products.length });
 };
 
+// static
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+  const search = "bed";
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  });
 
   res.status(StatusCodes.OK).json({ products, count: products.length });
 };
