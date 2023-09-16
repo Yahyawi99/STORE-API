2f46f880070ebf56ed221458eb9856a0b97a1ccd
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-16 19:09:29 +0100

    implement numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -2,10 +2,10 @@ const Product = require("../models/product");
 const { StatusCodes } = require("http-status-codes");
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, select } = req.query;
+  const { featured, company, name, sort, select, numericFilters } = req.query;
 
+  // search
   const queryObject = {};
-
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
@@ -34,11 +34,38 @@ const getAllProducts = async (req, res) => {
     result = result.select(selectList);
   }
 
+  // page and limit
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 5;
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
+
+  // Numerique filters
+  if (numericFilters) {
+    let filterObject = {};
+    const signObject = {
+      "<": "$lt",
+      ">": "$gt",
+      ">=": "$gte",
+      "<=": "$lte",
+      "=": "$eq",
+    };
+
+    const filtersArray = numericFilters.split(",");
+
+    filtersArray.forEach((filter) => {
+      const name = filter.match(/[a-z]/g).join("");
+
+      const sign = filter.match(/\W/g).join("");
+
+      const value = Number(filter.match(/\d/g).join(""));
+
+      filterObject[name] = { [signObject[sign]]: value };
+    });
+
+    result = result.find(filterObject);
+  }
 
   const products = await result;
 
